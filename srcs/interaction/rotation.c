e5b37b6237301d1fe9b42938037dd0e4b47834df
tnoulens <thibault.noulens@proton.me>
2023-01-09 15:42:16 +0100

    rotation test

--- srcs/interaction/rotation.c
+++ srcs/interaction/rotation.c
@@ -20,16 +20,16 @@ void	ft_rotation_x(void *obj)
 	float	dy;
 	float	dz;
 	t_scene	*scn;
-	t_sp	*sp;
 
 	scn = (t_scene *)obj;
 	dy = scn->cam.pos.y;
 	dz = scn->cam.pos.z;
 	scn->cam.pos.y = dy * cosf(+0.09f) + dz * sinf(+0.09f);
 	scn->cam.pos.z = -dy * sinf(+0.09f) + dz * cosf(+0.09f);
-	sp = (t_sp *)scn->objtab[0];
-	dy = sp->pos.y;
-	dz = sp->pos.z;
+	dy = scn->cam.orientation.y;
+	dz = scn->cam.orientation.z;
+	scn->cam.orientation.y = dy * cosf(+0.09f) + dz * sinf(+0.09f);
+	scn->cam.orientation.z = -dy * sinf(+0.09f) + dz * cosf(+0.09f);
 }
 
 void	ft_rotation_y(void *obj)
@@ -43,6 +43,10 @@ void	ft_rotation_y(void *obj)
 	dz = scn->cam.pos.z;
 	scn->cam.pos.x = dx * cosf(+0.09f) + dz * sinf(+0.09f);
 	scn->cam.pos.z = -dx * sinf(+0.09f) + dz * cosf(+0.09f);
+	dx = scn->cam.orientation.x;
+	dz = scn->cam.orientation.z;
+	scn->cam.orientation.x = dx * cosf(+0.09f) + dz * sinf(+0.09f);
+	scn->cam.orientation.z = -dx * sinf(+0.09f) + dz * cosf(+0.09f);
 }
 
 void	ft_rotation_z(void *obj)
@@ -56,4 +60,8 @@ void	ft_rotation_z(void *obj)
 	dy = scn->cam.pos.y;
 	scn->cam.pos.x = dx * cosf(+0.09f) - dy * sinf(+0.09f);
 	scn->cam.pos.y= dx * sinf(+0.09f) + dy * cosf(+0.09f);
+	dy = scn->cam.orientation.y;
+	dx = scn->cam.orientation.x;
+	scn->cam.orientation.x = dx * cosf(+0.09f) - dy * sinf(+0.09f);
+	scn->cam.orientation.y= dx * sinf(+0.09f) + dy * cosf(+0.09f);
 }
