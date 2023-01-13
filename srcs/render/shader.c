741145d708df4a778b6bc3c7d40eb3cf206cfa63
hyujung <jha0517@hotmail.com>
2023-01-13 11:12:52 +0000

    shader update

--- srcs/render/shader.c
+++ srcs/render/shader.c
@@ -6,7 +6,7 @@
 /*   By: hyunah <hyunah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 07:56:52 by hyunah            #+#    #+#             */
-/*   Updated: 2023/01/13 10:51:27 by hyunah           ###   ########.fr       */
+/*   Updated: 2023/01/13 11:12:11 by hyunah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,33 +59,33 @@ int	shadow_visibility(t_scene *scene, t_func *inter, t_surfaceinfo *info)
 
 int	lambert(t_scene *scene, t_surfaceinfo *info, int c_obj, t_func *inter)
 {
-	// float	f_ratio;
-	// int		vis;
-	// int		r;
-	// int		g;
-	// int		b;
-	t_vec3	result;
+	float	f_ratio;
+	int		vis;
+	int		r;
+	int		g;
+	int		b;
+	// t_vec3	result;
 	// t_vec3	color;
 	// int	colorint;
-	(void) info;
-	(void) scene;
-	(void) c_obj;
-	(void) inter;
+	// (void) info;
+	// (void) scene;
+	// (void) c_obj;
+	// (void) inter;
 	// color = vec_color(get_r(scene->alight.color), get_g(scene->alight.color), get_b(scene->alight.color));
 	// print_vec(&color);
 	// colorint = scene->alight.color;
 	// printf("%i %i %i\n", get_r(colorint), get_g(colorint), get_b(colorint));
-	result = vec_scale(vec_color(scene->alight.color), scene->alight.al);
-	// f_ratio = ft_max(0.0f, vec_dot(info->hit_normal, vec_normalize(vec_scale(scene->light.pos, -1))));
-	// vis = shadow_visibility(scene, inter, info);
-	// r = get_r(scene->objtab[c_obj]->metacolor) * f_ratio * scene->light.brightness * vis;
-	// if (r > 255)
-	// 	r = 255;
-	// g = get_g(scene->objtab[c_obj]->metacolor) * f_ratio * scene->light.brightness * vis;
-	// if (g > 255)
-	// 	g = 255;
-	// b = get_b(scene->objtab[c_obj]->metacolor) * f_ratio * scene->light.brightness * vis;
-	// if (b > 255)
-	// 	b = 255;
-	return (ft_trgb(255, result.x, result.y, result.z));
+	// result = vec_scale(vec_color(scene->alight.color), scene->alight.al);
+	f_ratio = ft_max(0.0f, vec_dot(info->hit_normal, vec_normalize(vec_scale(scene->light.pos, -1))));
+	vis = shadow_visibility(scene, inter, info);
+	r = get_r(scene->objtab[c_obj]->metacolor) * f_ratio * scene->light.brightness * vis;
+	if (r > 255)
+		r = 255;
+	g = get_g(scene->objtab[c_obj]->metacolor) * f_ratio * scene->light.brightness * vis;
+	if (g > 255)
+		g = 255;
+	b = get_b(scene->objtab[c_obj]->metacolor) * f_ratio * scene->light.brightness * vis;
+	if (b > 255)
+		b = 255;
+	return (ft_trgb(255, r, g, b));
 }
