bfd8467980e686cc0517a5862737f0bc82eea6ac
hyujung <jha0517@hotmail.com>
2023-01-23 14:35:58 +0000

    light. to lamp->

--- srcs/render/shader.c
+++ srcs/render/shader.c
@@ -6,7 +6,7 @@
 /*   By: hyunah <hyunah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 07:56:52 by hyunah            #+#    #+#             */
-/*   Updated: 2023/01/23 10:13:27 by hyunah           ###   ########.fr       */
+/*   Updated: 2023/01/23 14:33:46 by hyunah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	shading(t_scene *scene, t_surfaceinfo *info, int c_obj, t_func *inter)
 	t_vec3		obj_color;
 	t_vec3		light_dir;
 
-	light_intensity = scene->light.brightness;
+	light_intensity = scene->lamp->brightness;
 	obj_color = vec_color(scene->objtab[c_obj]->metacolor);
 	get_pointlight_info(scene, info, &light_dir, &light_intensity);
 	vis = shadow_visibility(scene, inter, info, light_dir);
@@ -28,7 +28,7 @@ int	shading(t_scene *scene, t_surfaceinfo *info, int c_obj, t_func *inter)
 	vec_scale(light_dir, -1)));
 	light_intensity *= scene->light.exposure;
 	mat.ambient = vec_scale(vec_mult(obj_color, vec_color(scene->alight.color)), scene->alight.al);
-	mat.diffuse = vec_scale(vec_mult(obj_color, vec_color(scene->light.color)), vis * mat.face_ratio * light_intensity);
+	mat.diffuse = vec_scale(vec_mult(obj_color, vec_color(scene->lamp->color)), vis * mat.face_ratio * light_intensity);
 	mat.result = vec_scale(vec_add(mat.ambient, mat.diffuse), 0.5);
 	return (int_color(mat.result));
 }
--- srcs/render/shader_bonus.c
+++ srcs/render/shader_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hyunah <hyunah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 07:56:52 by hyunah            #+#    #+#             */
-/*   Updated: 2023/01/23 11:37:51 by hyunah           ###   ########.fr       */
+/*   Updated: 2023/01/23 14:35:19 by hyunah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ int	shading(t_scene *scene, t_surfaceinfo *info, int c_obj, t_func *inter)
 	t_vec3		obj_color;
 	t_vec3		light_dir;
 
-	light_intensity = scene->light.brightness;
+	light_intensity = scene->lamp->brightness;
 	obj_color = vec_color(scene->objtab[c_obj]->metacolor);
 	get_pointlight_info(scene, info, &light_dir, &light_intensity);
 	light_intensity *= scene->light.exposure;
@@ -110,7 +110,7 @@ int	shading(t_scene *scene, t_surfaceinfo *info, int c_obj, t_func *inter)
 	if (scene->objtab[c_obj]->objtp == 2)
 		pattern = calcule_plan_pattern(info, &obj_color);
 	mat.ambient = vec_scale(vec_color(scene->alight.color), scene->alight.al);
-	mat.diffuse = vec_scale(vec_scale(vec_color(scene->light.color), \
+	mat.diffuse = vec_scale(vec_scale(vec_color(scene->lamp->color), \
 					vis * light_intensity * pattern), mat.face_ratio);
 	mat.specular = calcule_specular(light_dir, info, vis, light_intensity);
 	mat.result = vec_mult(vec_scale(vec_add(vec_add(mat.diffuse, mat.ambient), mat.specular), 1/3.0), obj_color);
